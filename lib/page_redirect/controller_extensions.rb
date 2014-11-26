d91ab5922e9c16ee7f6655cf7bdcbc0e7d1c9817
Michael Haag <michael.haag@ahorngmbh.de>
2014-11-26 10:50:28 +0100

    remove deprecated methods

--- lib/page_redirect/controller_extensions.rb
+++ lib/page_redirect/controller_extensions.rb
@@ -2,7 +2,7 @@ module PageRedirect::ControllerExtensions
   def self.included(base)
     base.class_eval { before_filter :apply_page_redirects }
   end
-  
+
   def apply_page_redirects
     url = params[:url]
     if Array === url
@@ -10,8 +10,8 @@ module PageRedirect::ControllerExtensions
     else
       url = url.to_s
     end
-    a_match = params[:locale].blank? ? Page.find_by_url(url, live?) : Page.find_by_url("#{params[:locale].to_s}/#{url}", live?)
-    if a_match && a_match.is_a?(RedirectPage) 
+    a_match = params[:locale].blank? ? Page.find_by_path(url, live?) : Page.find_by_path("#{params[:locale].to_s}/#{url}", live?)
+    if a_match && a_match.is_a?(RedirectPage)
       if redirect_url = a_match.redirect_url
         location = redirect_url.match('http://') || redirect_url.match('https://')  ? redirect_url : url_for(:controller => 'site', :action => 'show_page', :url => redirect_url)
         redirect_to CGI.unescape(location), :status => '307 Temporarily Moved' and return
