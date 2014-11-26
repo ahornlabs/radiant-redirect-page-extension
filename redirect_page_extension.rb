e4589b3705312a51d18d4962f085113abaee3b73
Michael Haag <michael.haag@ahorngmbh.de>
2014-11-26 10:52:39 +0100

    remove obsolete part

--- redirect_page_extension.rb
+++ redirect_page_extension.rb
@@ -2,18 +2,15 @@
 # require_dependency 'application'
 
 class RedirectPageExtension < Radiant::Extension
-  version "0.1"
-  description "A redirect page will redirect to the url defined in the body part"
-  url "http://github.com/p8/radiant-redirect-page-extension"
-  
+
   def activate
     SiteController.send :include, PageRedirect::ControllerExtensions
     Page.class_eval do
       include RedirectPageTags
     end
   end
-  
+
   def deactivate
   end
-  
+
 end
